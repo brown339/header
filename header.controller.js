1500c9bdc5d6998c61f95e076ece5c0fb8448a1a
John Brown <johndbrown91@gmail.com>
2018-08-02 09:45:11 -0400

    Base module

---
+++ header.controller.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('HeaderController', HeaderController);
+
+  HeaderController.$inject = [];
+  function HeaderController () {
+    var self = this;
+  }
+})();
